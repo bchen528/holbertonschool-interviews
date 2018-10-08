668016cf80677fddb9a7196fea529e9eb0eeb7c4
bchen528 <bchen720@gmail.com>
2018-10-08 17:08:11 +0000

    task 0 header

---
+++ 0x01-warm_up/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef __SEARCH_ALGOS__
+#define __SEARCH_ALGOS__
+#include <stdio.h>
+
+
+int binary_search(int *array, size_t size, int value);
+void print_array(const int *array, size_t start, size_t end);
+
+#endif /*__SEARCH_ALGOS__*/
